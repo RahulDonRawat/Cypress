5875898cec960a895f6e8af5652713335293769e
Rahul Rawat Flairlabs <rahul.rawat.flairlabs@ext.idfy.com>
2025-02-19 12:38:01 +0530

    test file

---
+++ cypress/e2e/Mock API/dummyMock2.cy.js
@@ -0,0 +1,22 @@
+describe('Fisrt test suite', function () {
+
+    it('Fisrst test case', function () {
+        cy.intercept
+            (
+                'GET',
+                'https://rahulshettyacademy.com/Library/GetBook.php?AuthorName=shetty',
+                (req) => {
+                    req.url = "https://rahulshettyacademy.com/Library/GetBook.php?AuthorName=Rawat"
+                    req.continue((res) => {
+
+                        expect(res.statusCode).to.equal(403)
+                    })
+                }
+            )
+    }).as("dummyURL")
+    cy.get("button[class='btn btn-primary']").click()
+    cy.wait('@dummyURL')
+
+
+
+})
