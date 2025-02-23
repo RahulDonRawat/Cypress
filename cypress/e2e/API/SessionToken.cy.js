04a6d2c1448622574f8ca67b58688c1f6f200ec2
Rahul Rawat Flairlabs <rahul.rawat.flairlabs@ext.idfy.com>
2025-02-23 18:50:39 +0530

    NEw change

--- cypress/e2e/API/SessionToken.cy.js
+++ cypress/e2e/API/SessionToken.cy.js
@@ -5,7 +5,26 @@ describe('Session token', function () {
                 onBeforeLoad: (window) => {
                     window.localStorage.setItem('token', Cypress.env('token'));
                 }
+
             });
+            cy.get('.card').first().find('.btn.w-10').click();
+            cy.contains("Cart").click()
+            cy.contains("Checkout").click()
+            cy.get('input[placeholder="Select Country"]').type('India');
+            cy.get('.ta-results button').each(($el) => {
+                if ($el.text() === 'India') {  // Check if the text is "India"
+                  cy.wrap($el).click();  // Click on the matching option
+                }
+              });
+              cy.get(".action_submit").click();
+
+
         });
     });
+
+
+
+
+
+
 });
